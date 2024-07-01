d773addc5199792165ab6284f9fec8b864804f28
AHMEDSAHEEL <shaheel123as@gmail.com>
2024-07-01 21:52:06 +0530

    Login and signup gender and phno added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function showLogin() {
     const formSlide = document.querySelector('.form-slide');
-    formSlide.style.transform = 'translateX(0)';
+    formSlide.style.transform = 'translateX(0%)';
 }
 
 function showSignup() {
