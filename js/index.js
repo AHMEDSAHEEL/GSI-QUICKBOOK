05b136770244b92bd9515365614e7c17edaa4adc
AHMEDSAHEEL <shaheel123as@gmail.com>
2024-07-01 19:07:36 +0530

    Login and signup page designed

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function showLogin() {
+    const formSlide = document.querySelector('.form-slide');
+    formSlide.style.transform = 'translateX(0)';
+}
+
+function showSignup() {
+    const formSlide = document.querySelector('.form-slide');
+    formSlide.style.transform = 'translateX(-51%)';
+}
